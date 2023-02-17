3bdc902038f48a87cbe03c1203c90cbbb80ce756
Alejandro Barreto <123841324+elAleDeArg@users.noreply.github.com>
2023-02-17 17:50:02 +0000

    tercer commit

--- .devcontainer/server/controlers/posts.controlers.js
+++ .devcontainer/server/controlers/posts.controlers.js
@@ -1,6 +1,7 @@
 import post from "../models/post.js";
 import Post from "../models/post.js";
-import { uploadImage } from "../libs/cloudinary.js";
+import { deleteImage, uploadImage } from "../libs/cloudinary.js";
+import fs from "fs-extra";
 
 export const getPost = async (req, res) => {
   try {
@@ -15,13 +16,19 @@ export const getPost = async (req, res) => {
 export const createPost = async (req, res) => {
   try {
     const { title, description } = req.body;
+    let image;
 
     if (req.files.image) {
-      const resultado =  await uploadImage(req.files.image.tempFilePath)
-      console.log(resultado)
+      const resultado = await uploadImage(req.files.image.tempFilePath);
+      console.log(resultado);
+      image = {
+        url: resultado.secure_url,
+        public_id: resultado.public_id,
+      };
+      await fs.remove(req.files.image.tempFilePath);
     }
 
-    const newPost = new Post({ title, description });
+    const newPost = new Post({ title, description, image });
 
     await newPost.save();
 
@@ -51,6 +58,9 @@ export const deletingPost = async (req, res) => {
 
     if (!postRemove) return res.sendStatus(404);
 
+    if (postRemove.image.public_id) {
+      await deleteImage(postRemove.public_id);
+    }
     return res.sendStatus(204);
   } catch (error) {
     console.error(error.message);
--- .devcontainer/server/libs/cloudinary.js
+++ .devcontainer/server/libs/cloudinary.js
@@ -1,7 +1,17 @@
 import {v2 as cloudinary} from 'cloudinary'
 
+cloudinary.config({
+    cloud_name:"dzxkjsvbt",
+    api_key: "865331813254283",
+    api_secret: "OO4bGnAGQFLluNP8Jn8mU1WE0IE"
+})
+
 export const uploadImage = async filePath =>{
-    return await cloudinary.uploader.upload(filePath {
+    return await cloudinary.uploader.upload(filePath, {
         folder: 'posts'
     })
+}
+
+export const deleteImage = async id => {
+    return await cloudinary.uploader.destroy(id)
 }
